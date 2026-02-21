2897afce9c5688d87cbf9fa5af9c7f2dc168058e
SiddheshSomvanshi <132122908+siddheshsomvanshi1@users.noreply.github.com>
2026-02-21 17:38:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,174 @@ provider "aws" {
   region = "ca-central-1"
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0938a60d87953e820"
-  instance_type = "t3.micro"
+# Get Available AZs
+data "aws_availability_zones" "available" {}
+
+# -----------------------
+# VPC
+# -----------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "jenkins-ec2"
+    Name                                = "main-vpc-eks"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
   }
+}
+
+# -----------------------
+# Public Subnets
+# -----------------------
+resource "aws_subnet" "public_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                = "public-subnet-${count.index}"
+    "kubernetes.io/cluster/eks-cluster" = "shared"
+    "kubernetes.io/role/elb"            = "1"
+  }
+}
+
+# -----------------------
+# Internet Gateway
+# -----------------------
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# -----------------------
+# Route Table
+# -----------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "a" {
+  count          = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# -----------------------
+# IAM Role for EKS Cluster
+# -----------------------
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-role-dev"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+# -----------------------
+# EKS Cluster
+# -----------------------
+resource "aws_eks_cluster" "cluster_eks" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.29"
+
+  vpc_config {
+    subnet_ids              = aws_subnet.public_subnet[*].id
+    endpoint_public_access  = true
+    endpoint_private_access = false
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
+}
+
+# -----------------------
+# IAM Role for Node Group
+# -----------------------
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group-role-dev"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+# -----------------------
+# EKS Managed Node Group
+# -----------------------
+resource "aws_eks_node_group" "main_nodes" {
+  cluster_name    = aws_eks_cluster.cluster_eks.name
+  node_group_name = "main-node-group"
+  node_role_arn   = aws_iam_role.nodes.arn
+  subnet_ids      = aws_subnet.public_subnet[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes_AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
