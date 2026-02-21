2c795658264e70a7b3d3ba358d1d8b36b4c88acb
SiddheshSomvanshi <132122908+siddheshsomvanshi1@users.noreply.github.com>
2026-02-21 18:08:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "eks_vpc" {
 resource "aws_subnet" "subnet_1" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
+  availability_zone = "ca-central-1a"
 
   tags = {
     Name = "eks-subnet-1"
@@ -26,7 +26,7 @@ resource "aws_subnet" "subnet_1" {
 resource "aws_subnet" "subnet_2" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-2b"
+  availability_zone = "ca-central-1b"
 
   tags = {
     Name = "eks-subnet-2"
